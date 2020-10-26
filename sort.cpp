f54ee77fdef7f67ac470df00ba3855b8f722dfd2
Rahul <70771921+rahulaxz@users.noreply.github.com>
2020-10-26 10:46:51 +0530

    Add files via upload

---
+++ sort.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+bool compare(int a,int b)
+{
+    return a>b;
+}
+int main(){
+int n,i;
+cin>>n;
+int a[1000];
+for(i=0;i<n;i++){
+
+    cin>>a[i];
+}
+sort(a,a+n,compare);
+for(i=0;i<n;i++)
+{
+    cout<<a[i]<<",";
+}
+return 0;
+
+
+
+}
